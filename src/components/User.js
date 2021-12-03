2b6e1c1f5d641277db57ab663a555108f31eb0e6
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-03 00:50:40 -0600

    Update User.js

---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class User extends React.Component {
+    render() {
+        const { user } = this.props;
+        
+        return(
+            <div id="usercard">
+                <img src={user.avatar_url} />
+                <a target="_blank" href={user.html_url} ><h3>{user.name}</h3></a>
+                <p>({user.login})</p>
+                <p>Total Repos: {user.public_repos}</p>
+                <p>Total Followers: {user.followers}</p>
+            </div>
+        );
+    }
+}
+
+export default User;
